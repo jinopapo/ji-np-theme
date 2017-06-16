d43eb495631c01ce168db382a5228367f2c0170c
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2017-06-16 22:43:48 +0900

    reset footer

--- footer.php
+++ footer.php
@@ -13,35 +13,31 @@
  */
 
 ?>
+            </div>
+                <footer id="colophon" class="site-footer" role="contentinfo">
+	            <?php
+	            if ( has_nav_menu( 'social' ) ) : ?>
+	                <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
+	                    <?php
+		                wp_nav_menu( array(
+		                    'theme_location' => 'social',
+    			            'menu_class'     => 'social-links-menu',
+	    	                'depth'          => 1,
+		 	                'link_before'    => '<span class="screen-reader-text">',
+	   	                    'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
+                        ) );
+	                    ?>
+	                </nav>
+	                <?php endif;
+                    //コピーライトとかのサイト情報のテンプレート呼び出し
+		            get_template_part( 'template-parts/footer/site', 'info' );
+	                ?>
+                </footer>
+	        </div>
+        </div>
+        <?php //プラグインが独自のjsを使っていたときに対応するために入れておく
+            wp_footer();
+        ?>
 
-		</div><!-- #content -->
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-				if ( has_nav_menu( 'social' ) ) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
-						?>
-					</nav><!-- .social-navigation -->
-				<?php endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
-				?>
-			</div><!-- .wrap -->
-		</footer><!-- #colophon -->
-	</div><!-- .site-content-contain -->
-</div><!-- #page -->
-<?php wp_footer(); ?>
-
-</body>
+    </body>
 </html>

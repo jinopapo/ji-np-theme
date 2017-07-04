306432555a650a02e960bc37e11d7f4e18d964c0
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2017-07-04 23:35:33 +0900

    rm gomi

--- template-parts/post/content-gallery.php
+++ template-parts/post/content-gallery.php
@@ -22,12 +22,7 @@
 		<?php
 		if ( 'post' === get_post_type() ) {
 			echo '<div class="entry-meta">';
-				if ( is_single() ) {
-					twentyseventeen_posted_on();
-				} else {
-					echo twentyseventeen_time_link();
-					twentyseventeen_edit_link();
-				};
+            twentyseventeen_edit_link();
 			echo '</div><!-- .entry-meta -->';
 		};
 
@@ -83,10 +78,5 @@
 
 	</div><!-- .entry-content -->
 
-	<?php
-	if ( is_single() ) {
-		twentyseventeen_entry_footer();
-	}
-	?>
 
 </article><!-- #post-## -->

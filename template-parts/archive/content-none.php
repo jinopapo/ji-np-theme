2d873205d0293bc1e6c82d7ad1cd84202269f93a
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2017-12-31 00:44:39 +0900

    タイポ修正

---
+++ template-parts/archive/content-none.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 投稿がないカテゴリーで表示されるテンプレート
+ *
+ * @package WordPress
+ * @subpackage ji-no-thme
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1>coming soon... </h1>
+	</header>
+</section><!-- .no-results -->

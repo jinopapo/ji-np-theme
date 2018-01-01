c566c373e0b8490b246ae7f6c13fe488a10a9eba
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2018-01-01 15:56:56 +0900

    ページ送りのリンクを削除

--- template-parts/archive/content-characters.php
+++ template-parts/archive/content-characters.php
@@ -53,11 +53,3 @@ while ( have_posts() ) : the_post();
 
 </article><!-- #post-## -->
 <?php endwhile;?>
-<?php
-#ページ送りのリンクを表示
-    the_posts_pagination( array(
-        'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-        'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-    ) );
-?>
